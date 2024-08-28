3669c23227ae8cac61d9dccc3dbabe7b98384631
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-08-28 20:47:42 +0300

    implement mongo db into controller

--- SkymeyAPIPost/Controllers/PostController.cs
+++ SkymeyAPIPost/Controllers/PostController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using SkymeyAPIPost.Data;
+using SkymeyLibs.Models.Tables.Posts;
 
 namespace SkymeyAPIPost.Controllers
 {
@@ -6,6 +9,8 @@ namespace SkymeyAPIPost.Controllers
     [Route("[controller]")]
     public class PostController : ControllerBase
     {
+        private static MongoClient _mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+        private static ApplicationContext _db = ApplicationContext.Create(_mongoClient.GetDatabase("skymey"));
         private readonly ILogger<PostController> _logger;
 
         public PostController(ILogger<PostController> logger)
@@ -13,10 +18,16 @@ namespace SkymeyAPIPost.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        [Route("Test")]
-        public string Get()
+        [HttpPost]
+        [Route("CreatePost")]
+        public async Task<string> CreatePost(API_POST POST)
         {
+            POST.API_URL = "url";
+            POST.Title = "title";
+            POST.Description = "description";
+            POST.Content = "content";
+            await _db.AddAsync(POST);
+            await _db.SaveChangesAsync();
             return "Ok";
         }
     }
---
+++ SkymeyAPIPost/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+using SkymeyLibs.Models.Tables.Posts;
+
+namespace SkymeyAPIPost.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<API_POST> API_POST { get; init; }
+        public static ApplicationContext Create(IMongoDatabase database) =>
+            new(new DbContextOptionsBuilder<ApplicationContext>()
+                .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+                .Options);
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<API_POST>().ToCollection("API_POST");
+        }
+    }
+}
